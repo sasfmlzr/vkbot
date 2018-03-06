56910e4ef67ea50f80474e1e030390295a29a47d
sasfmlzr <sasfmlzr@yandex.ru>
2018-03-06 23:28:18 +0300

    Refartor code

--- src/main/java/com/apiVKmanual/thread/ThreadUserBot.java
+++ src/main/java/com/apiVKmanual/thread/ThreadUserBot.java
@@ -1,14 +1,12 @@
 package com.apiVKmanual.thread;
 
-import com.api.client.Client;
+
 import com.apiVKmanual.client.BotApiClient;
 import com.apiVKmanual.object.UserRights;
-import com.fomenko.vkbot.controller.menuprogram.StatisticsWindowController;
 import com.vk.api.sdk.client.actors.UserActor;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.objects.messages.Dialog;
-import javafx.scene.chart.XYChart;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -16,7 +14,7 @@ import java.util.List;
 import java.util.Objects;
 
 import static com.apiVKmanual.object.StatisticsVariable.*;
-import static com.fomenko.vkbot.StaticModel.userBot;
+
 
 
 
@@ -37,7 +35,6 @@ public class ThreadUserBot extends ThreadBot implements Runnable 		//(содер
 
         while (!stoped) {
             boolean exception = false;
-                    actor=Client.actor;
             try {
                 sendMessageUser(actor);
             } catch (ClientException | SQLException   | InterruptedException | ApiException e) {
@@ -63,8 +60,9 @@ public class ThreadUserBot extends ThreadBot implements Runnable 		//(содер
     }
 
 
+
     //-----------------отправка сообщения, если есть непрочитанные-----------------//
-    public void sendMessageUser(UserActor actor) throws ClientException, ApiException, InterruptedException, SQLException {
+    private void sendMessageUser(UserActor actor) throws ClientException, ApiException, InterruptedException, SQLException {
 
         while (client.stateBot.pushPowerBot) {
             client.stateBot.findMessage = false;        // совпадение с сообщением не найдено
@@ -77,8 +75,8 @@ public class ThreadUserBot extends ThreadBot implements Runnable 		//(содер
             String message;         // сообщение бота
             String obrachenie = "Колян, ";                      //обращение к боту
 
-            message = userBot.botApiClient().database.getBotRandomData().
-                    get(userBot.botApiClient().other().randomId(userBot.botApiClient().database.
+            message = client.database.getBotRandomData().
+                    get(client.other().randomId(client.database.
                             getBotRandomData().size())).response;          //сообщение берется из рандомной базы коляна
                         /*
            if (textMessageString.equals("")){
@@ -95,9 +93,9 @@ public class ThreadUserBot extends ThreadBot implements Runnable 		//(содер
 
             if (messagesList.size() != 0) {
                 int userID =  messagesList.get(0).getMessage().getUserId();               // запись userID пользователя
-                userBot.botApiClient().database.databaseRequest(userBot.botApiClient().database.getStatmt()).addInfoUser(userID, actor, userBot.getVk());       // добавить инфу о пользователе, если нет  // здесь есть запрос к вк
-                userBot.botApiClient().database.databaseRequest(userBot.botApiClient().database.getStatmt()).addInfoUserRights(userID, actor);    // добавить права пользователю, если нет
-                String userRightString = userBot.botApiClient().database.databaseRequest(userBot.botApiClient().database.getStatmt()).findUserRights(userID, actor);  // запись права текущего пользователя в ячейку
+                client.database.databaseRequest(client.database.getStatmt()).addInfoUser(userID, actor, client.getVk());       // добавить инфу о пользователе, если нет  // здесь есть запрос к вк
+                client.database.databaseRequest(client.database.getStatmt()).addInfoUserRights(userID, actor);    // добавить права пользователю, если нет
+                String userRightString = client.database.databaseRequest(client.database.getStatmt()).findUserRights(userID, actor);  // запись права текущего пользователя в ячейку
                 userRight = new UserRights(userRightString);  // наследование прав пользователем
                 //    System.out.print(userRight.getNameRight() + " может админить? -" + userRight.getAdminCommands() + "\n" +
                 //           userRight.getNameRight() + " может писать боту? -" + userRight.getAllowWriteToBot() + "\n");
@@ -112,9 +110,9 @@ public class ThreadUserBot extends ThreadBot implements Runnable 		//(содер
                             textMessageString = textMessageString.replaceAll(obrachenie.toLowerCase(), "");
                             textMessageString = textMessageString.replaceAll("[^ A-Za-zА-Яа-я0-9?]", "");       // замена знаков
                             String dublicateMessages=message;
-                            message = userBot.botApiClient().messages().commands().adminCommandsBot(textMessageString, message);         //проверка на команды бота
+                            message = client.messages().commands().adminCommandsBot(textMessageString, message);         //проверка на команды бота
                             if (!Objects.equals(message, dublicateMessages)) {
-                                userBot.botApiClient().messages().vkSendMessage(actor, message, messagesList);
+                                client.messages().vkSendMessage(actor, message, messagesList);
                                 messagesList.clear();
                             }
                         }
@@ -136,14 +134,14 @@ public class ThreadUserBot extends ThreadBot implements Runnable 		//(содер
                     if (!client.stateBot.findMessage) {     // если совпадение с сообщением не найдено, то
                         if (textMessageString.contains(obrachenie.toLowerCase())) {
                             textMessageString = textMessageString.replaceAll(obrachenie.toLowerCase(), "");
-                            message = userBot.botApiClient().messages().commands().commandsBot(textMessageString, message, actor, messagesList, userBot.botApiClient());         //проверка на команды бота
+                            message = client.messages().commands().commandsBot(textMessageString, message, actor, messagesList, client);         //проверка на команды бота
                             textMessageString = textMessageString.replaceAll("[^ A-Za-zА-Яа-я0-9?]", "");       // замена знаков
                         }
                     }
                     if (!client.stateBot.reduction) {
                         message = messageFromDataBase(textMessageString, message);       // бд коляна
                         message = messageFromBigDataBase(textMessageString, message);    // большая бд
-                        userBot.botApiClient().messages().vkSendMessage(actor, message, messagesList);       // отправка сообщения и пометка его прочитанным
+                        client.messages().vkSendMessage(actor, message, messagesList);       // отправка сообщения и пометка его прочитанным
                     } else {
                         Thread.sleep(500);
                         //            System.out.print("Нет новых сообщений" + "\n");
@@ -167,18 +165,18 @@ public class ThreadUserBot extends ThreadBot implements Runnable 		//(содер
             }
 
 /////////////////////////статистика
-            StatisticsWindowController.seriesZaprosVk.getData().add(new XYChart.Data(countSendMessageUser, timeZaprosFinishItogo));          //ведение статистики запросов
-            StatisticsWindowController.seriesItogVk.getData().add(new XYChart.Data(countSendMessageUser, timeItogoMsMinusVK));          //ведение статистики запросов
-            StatisticsWindowController.seriesThread.getData().add(new XYChart.Data(countSendMessageUser, timeDelayThread));                        //ведение статистики задержки потока////здесь иногда ловится исключение
+            //StatisticsWindowController.seriesZaprosVk.getData().add(new XYChart.Data(countSendMessageUser, timeZaprosFinishItogo));          //ведение статистики запросов
+            //StatisticsWindowController.seriesItogVk.getData().add(new XYChart.Data(countSendMessageUser, timeItogoMsMinusVK));          //ведение статистики запросов
+            //StatisticsWindowController.seriesThread.getData().add(new XYChart.Data(countSendMessageUser, timeDelayThread));                        //ведение статистики задержки потока////здесь иногда ловится исключение
         }
-        userBot.botApiClient().database.CloseDB();         //закрытие бд
+        client.database.CloseDB();         //закрытие бд
         client.stateBot.botWork = false;
     }
 
     //-----------------отправка и отслеживание запроса в вк на непрочитанные сообщения-------------------------------//
     private List<Dialog> createListMessageVK() throws ClientException, ApiException {
         long timezaprosstart =       System.currentTimeMillis();         // начало запроса непрочитанного запроса
-        List<Dialog> messages = userBot.getVk().messages().getDialogs(userBot.getActor())                 // Листы сообщений
+        List<Dialog> messages = client.getVk().messages().getDialogs(actor)                 // Листы сообщений
                 .unread(true)
                 .execute().getItems();
         long timezaprosfinish = System.currentTimeMillis();
@@ -192,14 +190,14 @@ public class ThreadUserBot extends ThreadBot implements Runnable 		//(содер
         if (!client.stateBot.findMessage){      // если совпадение с сообщением не найдено, то
             long timeStartBD =       System.currentTimeMillis();
             // путешествие по списку объектов из БД
-            for (int countDB = 0; countDB <= userBot.botApiClient().database.getBotData().size() - 1; countDB = countDB + 1) {
-                if ( userBot.botApiClient().database.getBotData().get(countDB).request.toLowerCase().equals(textMessageString.toLowerCase()))  {  // сравниваем нижний регистр
-                    listMessages.add(userBot.botApiClient().database.getBotData().get(countDB).response);
+            for (int countDB = 0; countDB <= client.botApiClient().database.getBotData().size() - 1; countDB = countDB + 1) {
+                if ( client.database.getBotData().get(countDB).request.toLowerCase().equals(textMessageString.toLowerCase()))  {  // сравниваем нижний регистр
+                    listMessages.add(client.database.getBotData().get(countDB).response);
                     client.stateBot.findMessage=true;                                                           // совпадение с сообщением найдено
                 }
             }
             if (client.stateBot.findMessage)    // если совпадение с сообщением найдено, то
-                messages = listMessages.get(userBot.botApiClient().other().randomId(listMessages.size()));    // выбираем рандомно из найденного сообщение
+                messages = listMessages.get(client.other().randomId(listMessages.size()));    // выбираем рандомно из найденного сообщение
             long timeFinishBD =      System.currentTimeMillis();
             timeConsumedMillisBD = timeFinishBD - timeStartBD;
             countUsedBD=countUsedBD+1;                                              // количество использований бд коляна увеличилось на 1
@@ -231,22 +229,22 @@ public class ThreadUserBot extends ThreadBot implements Runnable 		//(содер
                 messages = listMessages.get(randomIdBot(listMessages.size()));    // выбираем рандомно из найденного сообщение
 */
 
-            for (int countDB = 0; countDB <= userBot.botApiClient().database.getBigMessagesData().size() - 1; countDB = countDB + 1) {
-                if ( userBot.botApiClient().database.getBigMessagesData().get(countDB).request.toLowerCase().equals(textMessageString))  {  // сравниваем нижний регистр
+            for (int countDB = 0; countDB <= client.database.getBigMessagesData().size() - 1; countDB = countDB + 1) {
+                if ( client.database.getBigMessagesData().get(countDB).request.toLowerCase().equals(textMessageString))  {  // сравниваем нижний регистр
                     // сравниваем сообщение и значение в БД
-                    listMessages.add(userBot.botApiClient().database.getBigMessagesData().get(countDB).response);
+                    listMessages.add(client.database.getBigMessagesData().get(countDB).response);
                     client.stateBot.findMessage=true;                                                   // совпадение с сообщением найдено
                     // messages = listMessages.get(randomIdBot(listMessages.size()));    // выбираем рандомно из найденного сообщение
                 }
             }
             if (client.stateBot.findMessage)                                                            // если совпадение с сообщением найдено, то
             {
-                messages = listMessages.get(userBot.botApiClient().other().randomId(listMessages.size()));          // выбираем рандомно из найденного сообщение
+                messages = listMessages.get(client.other().randomId(listMessages.size()));          // выбираем рандомно из найденного сообщение
             }
             long timeFinishBigBD =      System.currentTimeMillis();
             timeConsumedMillisBigBD = timeFinishBigBD - timeStartBigBD;
             countUsedBigBD=countUsedBigBD+1;                                            // количество использований большой бд увеличилось на 1
-            StatisticsWindowController.seriesBigBD.getData().add(new XYChart.Data(countUsedBigBD, timeConsumedMillisBigBD));                        //ведение статистики задержки потока////здесь иногда ловится исключение
+            //StatisticsWindowController.seriesBigBD.getData().add(new XYChart.Data(countUsedBigBD, timeConsumedMillisBigBD));                        //ведение статистики задержки потока////здесь иногда ловится исключение
         }
         return messages;
     }
